d1f4fe7830cf2ff6ea783ff1def6f45f0e450e75
Степан <bokarevstepan1@yandex.ru>
2021-10-05 19:38:06 +0300

    Выполнено первое задание 1-ого варианта

--- LabWork2SearchElement/LabWork2SearchElement.cpp
+++ LabWork2SearchElement/LabWork2SearchElement.cpp
@@ -11,7 +11,7 @@
 using namespace std;
 
 void FillRand(int* arr, int n, double min, double max); // Функция, формирующая рандомную последовательность
-void FillGrow(int* arr, int length, int min, int max); // Функция, формирующая возрастающую последовательность
+void Sort(int* arr, int length); // Функция, сортирующая последовательность
 void Writer(int* A, int length); // Функция, печатающая последовательность
 int QSearch(int* A, int x, int n); // Функция быстрого последовательного поиска в лекциях Sentinel_Linear_Search 
 int SSearch(int* A, int x, int n); // Функция последовательного поиска в лекциях Better_Linear_Search
@@ -21,118 +21,102 @@ int BSearch(int* A, int x, int n); // Функция бинарного поис
 int main()
 {
     setlocale(LC_ALL, "Russian");
+    system("color F0");
     srand(time(NULL));
     int n = 0;
     cout << "Введите размер последовательности: "; cin >> n;
     int length = n += 1;
     
     cout << "Рандомная последовательность" << endl;
+
     int* arrRand = new int[length]; // рандомная последовательность
     FillRand(arrRand, n, 0, 600000);
-    //int keyRand; cout << "Введите ключ поиска в рандомной последовательности: "; cin >> keyRand;
-
-    int* arrGrow = new int[length];
-    cout << "Возрастающая последовательность" << endl;
-    FillGrow(arrGrow, length, 0, length); // заполняем массив возрастающей последовательностью 
-    //int keyGrow; cout << "Введите ключ поиска в возрастающей последовательности: "; cin >> keyGrow;
-    
     
     int (*SearchOperations[4])(int*, int, int) = { QSearch, SSearch, TSearch, BSearch }; // Указатель на массив функций
     
     int lengthLin = sizeof(SearchOperations) / sizeof(SearchOperations[0]);// получаем длину массива
     //int keyRand; cout << "Введите ключ поиска в рандомной последовательности: "; cin >> keyRand;
-
     //int keyGrow; cout << "Введите ключ поиска в возрастающей последовательности: "; cin >> keyGrow;
     for (int i = 0; i < lengthLin - 2; i++) // цикл по рандомной последовательности
     {
         for (int j = 0; j < 3; j++) {
             int key = 0;
-            //key = keyRand;
+
             if (j == 0) key = arrRand[0]; // ключ поиска
             else if (j == 1) key = arrRand[length / 2]; // ключ поиска
             else if (j == 2) key = arrRand[length - 1]; // ключ поиска*/
             auto begin = chrono::steady_clock::now(); // получаем время перед началом формирования последовательности
-            //cout << "Процедура " << i << " ключ поиска : " << key << endl;
             
-
             int result = SearchOperations[i](&arrRand[0], key, length);    // вызов функции по указателю
-
 
             auto end = chrono::steady_clock::now();// получаем время по окончанию формирования последовательности
 
             auto elapsed_ms = chrono::duration_cast<chrono::microseconds>(end - begin);// получаем время работы в микросекундах
             if (result >= 0) {
-                //cout << "Процедура " << i << " индекс равен : " << result << endl;
+                cout << "Процедура " << i << " индекс равен : " << result << endl;
             }
             else {
-                //cout << "Процедура " << i << " элемент не найден" << endl;
+                cout << "Процедура " << i << " элемент не найден" << endl;
             }
-            cout << elapsed_ms.count() << endl; // вывод времени
+            //cout << elapsed_ms.count() << endl; // вывод времени
         }
     }
-
+    Sort(arrRand, length);
     for (int i = 2; i < lengthLin; i++) // цикл по возрастающей последовательности
     {
         for (int j = 0; j < 3; j++) {
             int key = 0;
-            if (j == 0) key = arrGrow[0]; // ключ поиска
-            else if (j == 1) key = arrGrow[length / 2]; // ключ поиска
-            else if (j == 2) key = arrGrow[length - 1]; // ключ поиска*/
+            if (j == 0) key = arrRand[0]; // ключ поиска
+            else if (j == 1) key = arrRand[length / 2]; // ключ поиска
+            else if (j == 2) key = arrRand[length - 1]; // ключ поиска*/
             auto begin = chrono::steady_clock::now(); // получаем время перед началом формирования последовательности
             //cout << "Процедура " << i << " ключ поиска : " << key << endl;
             //key = keyGrow;
-            int result = SearchOperations[i](&arrGrow[0], key, length);    // вызов функции по указателю
+            int result = SearchOperations[i](&arrRand[0], key, length);    // вызов функции по указателю
                 
           
             auto end = chrono::steady_clock::now();// получаем время по окончанию формирования последовательности
 
             auto elapsed_ms = chrono::duration_cast<chrono::microseconds>(end - begin);// получаем время работы в микросекундах
             if (result >= 0) {
-                //cout << "Процедура " << i << " индекс равен : " << result << endl;
+                cout << "Процедура " << i << " индекс равен : " << result << endl;
             }
             else {
-                //cout << "Процедура " << i << " элемент не найден" << endl;
+                cout << "Процедура " << i << " элемент не найден" << endl;
             }
-            cout << elapsed_ms.count() << endl; // вывод времени
+            //cout << elapsed_ms.count() << endl; // вывод времени
         }
     }
 
     
 
 }
-int SSearch(int* arr, int key, int size) {
+int SSearch(int* arr, int key, int size) { // реализован по блоксхеме ✔️
     
     int count = 0;
     for (int i = 0; i < size; i++) {
-        count++;
+        count += 2; // две операции сравнения, первая не вышли ли за размер массива, вторая на равенство ключу
         if (arr[i] == key) {
             cout << "Количество операций сравнения для SSearch " << count << endl;
             return i;
         }
-        if (arr[i] == arr[size - 1]) {
-            cout << "Количество операций сравнения для SSearch " << count << endl;
-            return NotFound;
-        }
     }
+    cout << "Количество операций сравнения для SSearch " << count << endl;
+    return NotFound;
 
 }
-int QSearch(int* arr, int key, int size) {
+int QSearch(int* arr, int key, int size) { // реализован по блоксхеме ✔️
     int i = 0;
     int count = 0;
     int last = arr[size];
     arr[size] = key;
     for (i; arr[i] != key; i++) {
-        count++;
+        count++; // одно сравнение, чтобы найти ключ поиска
     }
     arr[size] = last;
-    if (i < size) {
-        cout << "Кол-во сравнений QSearch = " << count << endl;
+    if (i < size || arr[size] == key) {
+        cout << "Кол-во сравнений QSearch = " << count << endl;        
         return i;
-    }
-    else if (arr[size] == key) { 
-        count++;
-        cout << "Кол-во сравнений QSearch = " << count << endl;
-        return i; 
     }
     else { 
         cout << "Кол-во сравнений QSearch = " << count << endl; 
@@ -140,34 +124,38 @@ int QSearch(int* arr, int key, int size) {
     }
     
 }
-int TSearch(int* arr, int key, int size) {
-    int i = 0;
-    int count = 0;
-    while (key > arr[i]) {
-        i++;
-        count++;
+int TSearch(int* arr, int key, int size) {// реализован по блоксхеме ✔️
+    int i = 0; // текущий индекс
+    int count = 0; // счетчик сравнений
+    while (key > arr[i]) { // увеличиваем текущий индекс до тех пор, пока ключ будет 
+        i++; //меньше или равен i-ому элементу в возраст. массиве
+        count++; // прибавляем один, тк выполняется одна операция сравнения
     }
     cout << "Кол-во сравнений TSearch = " << count << endl;
-    if (key == arr[i]) return i;
-    else return NotFound; 
+    if (key == arr[i]) { 
+        return i;
+    }
+    else {
+        return NotFound;
+    }
 }
-int BSearch(int* arr, int key, int size) {
-    int count = 0;
-    int p = 0; //Первый элемент массива
-    int r = size - 1;
-    while (p <= r) {
-        int q = (p + r) / 2;
+int BSearch(int* arr, int key, int size) {// реализован по блоксхеме ✔️ 
+    int count = 0; // счетчик сравнений
+    int p = 0; // Первый элемент массива
+    int r = size - 1; // последний элемент массива
+    while (p <= r) { // до тех пор пока начальная граница меньше конечной границы
+        int q = (p + r) / 2; // серединный элемент
         if (arr[q] == key) {
-            count++;
+            count += 2;
             cout << "Кол-во сравнений BSearch = " << count << endl;
             return  q;
         }
-        else if (arr[q] > key) {
-            count++;
+        else if (arr[q] > key) { // ключ поиска меньше, значения серединного элемента, уменьшаем правую границу
+            count +=2;
             r = q - 1;
         }
-        else {
-            count++;
+        else { // ключ поиска больше или равен значения серединного элемента, увеличиваем левую границу
+            count += 2;
             p = q + 1;
         }
     }
@@ -175,26 +163,30 @@ int BSearch(int* arr, int key, int size) {
     return NotFound;
 }
 
-void FillGrow(int* arr, int length, int min, int max) {
-    max--;
-    int step = 1 + (max - min) / length;
 
-    for (int i = 0; i < length - 1; i++) {
-        arr[i] = min + step * i;
-    }
-    //arr[min + rand() % (max - min + 1)] = arr[min + rand() % (max - min + 1)]; // повторяющийся элемент
-    arr[length - 1] = INT_MAX;
-    //Writer(arr, length);
-}
 void FillRand(int* arr, int length, double min, double max) {
     for (int i = 0; i < length - 1; i++) {
         arr[i] = 10 + ((double)rand() / RAND_MAX) * (max - min);
     }
     arr[length - 1] = INT_MAX;
-    //Writer(arr, length);
+    Writer(arr, length);
 }
 void Writer(int* A, int length) {
     for (int i = 0; i < length; i++) {
         cout << "arr[" << i << "] = " << A[i] << endl;
+    }
+}
+void Sort(int* A, int n)
+{
+    int* B = A;
+    for (int i = 0; i < n - 1; ++i)
+    {
+        int smallest = i;
+        for (int j = i + 1; j < n; ++j)
+        {
+            if (A[j] < A[smallest])
+                smallest = j;
+        }
+        swap(A[i], A[smallest]);
     }
 }
